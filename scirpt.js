1f3e9f170ebba41614e979ec22bebb324ce12dc1
Manthan_Manalwar <mmanalwar@gmail.com>
2023-10-01 08:17:17 +0530

    added all

---
+++ scirpt.js
@@ -0,0 +1,11 @@
+// // Get a reference to the "Rooms" button
+// const roomsButton = document.getElementById("rooms-button");
+
+// // Function to open the room options page
+// function openRoomOptionsPage() {
+//     // Navigate to the room-options.html page
+//     window.location.href = "room-options.html";
+// }
+
+// // Add a click event listener to the "Rooms" button
+// roomsButton.addEventListener("click", openRoomOptionsPage);
